4139f66bfc226b31acee07d3c259914b33103e2d
fabe <devnull@localhost>
2012-10-30 22:32:25 +0100

    refined address model

--- applications/bikesurfing/models/address.py
+++ applications/bikesurfing/models/address.py
@@ -4,7 +4,7 @@
 db.define_table(
     'address',
     
-    Field('street', 'string'),
+    Field('street', 'string'), # only visible if user is borrowing a bike
     Field('city' 'string'),
     Field('postalcode' 'string'),
     Field('country' 'string'),
@@ -20,4 +20,21 @@ db.define_table(
     plural = 'Addresses',
 )
 
-# TODO bike location, organisation locations
+db.define_table( 
+    'address_owner',
+    
+    Field('address_id', db.address),
+    Field('user_id', db.auth_user),
+    Field('organisation_id', db.organisation),
+    Field('bike_id', db.bike),
+    
+    Field('created_on','datetime',default=request.now,
+          label=T('Created On'),writable=False,readable=False),
+    Field('modified_on','datetime',default=request.now,
+          label=T('Modified On'),writable=False,readable=False,
+          update=request.now),
+          
+    format = '%(address_id)s:',
+    singular = 'AddressOwner',
+    plural = 'AddressOwners',
+)
