47567cc43a806c7172cc1346f8616a7eb2b7f264
fabe <devnull@localhost>
2013-12-31 10:11:11 +0100

    cleaned up statusbar a bit

--- apps/common/shortcuts.py
+++ apps/common/shortcuts.py
@@ -50,11 +50,12 @@ def render_response(request, template, args):
         account = request.user.accounts.all()[0]
         borrows = Borrow.objects.filter(borrower=account, active=True)
         borrows = borrows.exclude(state="CANCELED").exclude(state="FINISHED")
+        stations = Station.objects.filter(responsible=account)
         args.update({ 
             "current_account" : account,
             "borrow_count" : len(borrows),
             "current_account_teams" : team_control.get_teams(account),
-            "station_count" : len(Station.objects.filter(responsible=account)),
+            "current_account_stations" : stations,
             "departure_count" : len(borrow_control.departures(account)),
             "arrival_count" : len(borrow_control.arrivals(account)),
         })
